dc9688885d58634db8bee940b370eac2a24c2b6f
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2019-04-02 15:01:53 +0200

    XsrfListenerEventSubscriber added

---
+++ src/Event/XsrfListenerEventSubscriber.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class XsrfListenerEventSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var CsrfTokenManagerInterface
+     */
+    private $manager;
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'kernel.request' => array('onKernelRequest', 1000),
+            'kernel.response' => 'onKernelResponse'
+        );
+    }
+
+    public function __construct(CsrfTokenManagerInterface $provider)
+    {
+        $this->manager = $provider;
+    }
+
+    protected function refreshToken(FilterResponseEvent $e) {
+        $e
+            ->getResponse()
+            ->headers
+            ->setCookie(
+                new Cookie(
+                    'xsrf-token',
+                    $this->manager->refreshToken('archiraq'),
+                    0,
+                    '/',
+                    null,
+                    false,
+                    false)
+            );
+    }
+
+    public function onKernelRequest(GetResponseEvent $e)
+    {
+        if (in_array($e->getRequest()->getMethod(), array('POST', 'PUT', 'DELETE', 'PATCH'))) {
+            $token = $this->manager->getToken('archiraq');
+            if (
+                $token
+                && !$this->manager->isTokenValid($token)
+                || $token->getValue() !== $e->getRequest()->headers->get('x-xsrf-token')
+            ) {
+                $e->setResponse(new Response('The XSRF token is invalid', 412));
+                return;
+            }
+        }
+    }
+
+    public function onKernelResponse(FilterResponseEvent $e)
+    {
+        if (
+            $e->getRequest()->isMethod('GET') && $e->getRequest()->getPathInfo() === '/'
+            || $e->getRequest()->isMethod('POST') && $e->getRequest()->getPathInfo() === '/logout'
+        ) {
+            $this->refreshToken($e);
+        }
+    }
+}
---
+++ tests/tdd/Functional/Event/XsrfListenerEventSubscriberTest.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Tests\Functional\Event;
+
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+class XsrfListenerEventSubscriberTest extends WebTestCase
+{
+    /**
+     * @var Client
+     */
+    private static $client;
+
+    public static function setUpBeforeClass()
+    {
+        self::$client = self::createClient();
+    }
+
+    public function refreshingTokenUrlDataProvider()
+    {
+        return [
+          ['/']
+        ];
+    }
+
+    /**
+     * @dataProvider refreshingTokenUrlDataProvider
+     * @param string $url
+     */
+    public function testRequestingUrlWillRefreshXsrfToken(string $url)
+    {
+        self::$client->request('GET', $url);
+        $response = self::$client->getResponse();
+        $cookies = $response->headers->getCookies(ResponseHeaderBag::COOKIES_ARRAY);
+        $this->assertArrayHasKey( '', $cookies);
+        $this->assertArrayHasKey( '/', $cookies['']);
+        $this->assertArrayHasKey( 'xsrf-token', $cookies['']['/']);
+    }
+}
