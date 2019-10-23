aa62c3ebf15ec0afd3377e78c3cbbf84af5e4084
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2019-10-23 09:21:09 +0200

    WfsDataCardMx refactored

--- assets/js/src/mixins/WfsDataCardMx.js
+++ assets/js/src/mixins/WfsDataCardMx.js
@@ -1,45 +1,27 @@
-import {GET_GUEST_AUTH} from '@/store/geoserver/auth/getters';
 import DataCardMx from '@/mixins/DataCardMx';
-import {getFeatureQueryString, setWfsGetFeatureRequestHeaders, mapWfsFeatureToTableItem} from '@/utils/wfs';
+import WfsGetFeatureMx from '@/mixins/WfsGetFeatureMx';
+import {mapWfsFeatureToTableItem} from '@/utils/wfs';
 
 export default {
     mixins: [
-        DataCardMx
+        DataCardMx,
+        WfsGetFeatureMx
     ],
-    computed: {
-        geoserver() {
-            return {
-                auth: {
-                    guest: this.$store.getters[`geoserver/auth/${GET_GUEST_AUTH}`]
-                },
-                baseUrl: this.$store.state.geoserver.baseUrl
-            };
-        }
-    },
     methods: {
-        getUrl() {
-            return  this.geoserver.baseUrl + 'wfs?' +
-                getFeatureQueryString({
-                    typename: this.typename,
-                    filter: this.filter,
-                    pagination: this.pagination
-                });
-        },
         //@TODO check exceptions
         fetch: async function () {
-            let axiosRequestConfig = {
-                method: 'get',
-                url: this.getUrl(),
-                headers: setWfsGetFeatureRequestHeaders({}, this.geoserver.auth.guest)
+            let wfsRequestConfig = {
+                typename: this.typename,
+                filter: this.filter,
+                pagination: this.pagination
             };
-            try {
-                this.isRequestPending = true;
-                const response = await this.clientRequest(axiosRequestConfig);
-                this.items = response.data.features.map(mapWfsFeatureToTableItem);
-                this.totalItems = response.data.numberMatched;
-            } finally {
-                this.isRequestPending = false;
-            }
+
+            this.performWfsGetFeatureRequest(wfsRequestConfig).then(
+                (response) => {
+                    this.items = response.data.features.map(mapWfsFeatureToTableItem);
+                    this.totalItems = response.data.numberMatched;
+                }
+            );
         },
     },
 };
